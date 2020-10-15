cddaa58c3a0da17ec4cf50a524d43e0bfc29e6e9
Edson Januário <edson_2010_alves@hotmail.com>
2020-10-15 20:34:28 -0300

    05

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-/* IMPORTAÇÃO DO PACOTE EPRESS PARA INDEX  */
-// DECLARAR UMA CONSTANTE
-const express = require("express")
-
+/* IMPORTAÇÃO DO PACOTE EXPRESS PARA A INDEX */
+// DECLARAR UM CONSTANTE
+const express = require('express')
+ 
+/*INSTANCIA DE PACOTE*/
 const app = express()
-    
-
+app.use(express.static("public"));
+ 
+app.get("/", function(request, response){
+ 
+ console.log('DEU CERTO!!!!')
+ 
+});
+ 
 /*-------------------------------------------------------------------------------------------*/
 /*ÁREA DE TESTE*/
  
-console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS 01')
-console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS -2')
-
+// console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS')
+// console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS - 2')
+// console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS - 3')
+// console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS - 4')
+// console.log('TESTE DE SAÍDA DA APLICAÇÃO NODEJS - 5')
+ 
 /*-------------------------------------------------------------------------------------------*/
-
-
-
-
-
-
-app.listen(app.get("3000"), ()=>{
- console.log("servidor rodando")
-
+ 
+/* CRIAÇÃO DO SERVIDO WEB DA APLICAÇÃO */
+app.listen('3000', ()=>{
+ 
+ console.log('SERVIDOR RODANDO')
+ 
 })
